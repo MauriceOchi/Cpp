b6b450cc1e0561d2761acf7cca39f402be8b935c
Maurice Ochieng <63201775+MauriceOchi@users.noreply.github.com>
2024-01-06 18:54:54 +0300

    Update array_1.cpp

--- array_1.cpp
+++ array_1.cpp
@@ -6,7 +6,7 @@ int main(){
     testArray[0] = 45;
     testArray[1] = 1;
     testArray[2] = 3;
-    testArray[3] = 99;
+    testArray[3] = 20;
 
     cout << testArray[0] << " " << testArray[1] << " " << testArray[2] << " " << testArray[3] 
     << endl;
